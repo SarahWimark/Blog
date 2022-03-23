85fa5c643d85a842d6c231db05abe859e092bb90
sarahwimark <sarahwimark@gmail.com>
2022-03-23 20:52:18 +0100

    Adding nav in header

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sv">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8"/>
+    <link href="style.css" rel="stylesheet">
+    <title><?php echo TITLE; ?></title>
+
 </head>
 <body>
+<div class="main_container">
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </nav>
     
