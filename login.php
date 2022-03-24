e8746b1ad6988ae09248482b81a19ce393c6a773
sarahwimark <sarahwimark@gmail.com>
2022-03-24 16:12:28 +0100

    Working on header styles

--- login.php
+++ login.php
@@ -4,22 +4,22 @@
 
 <main>
     <div class="container w-25 mt-5 shadow p-4">
-    <form action="index.php" method="post">
-        <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" name="username">
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" name="password">
-        </div>
-        <div class="mt-2">
-        <button type="submit" class="btn btn-success" name="submit">Login</button>
-        </div>
-    </form>
-    <div><a href="signup.php">Signup instead?</a></div>
-    <div><a href="#">Forgot password?</a></div>
-    
+        <form action="index.php" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" name="username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <div class="mt-2">
+                <button type="submit" class="btn btn-success" name="submit">Login</button>
+            </div>
+        </form>
+        <div><a href="signup.php">Signup instead?</a></div>
+        <div><a href="#">Forgot password?</a></div>
+
     </div>
 </main>
 
--- src/includes/header.php
+++ src/includes/header.php
@@ -1,18 +1,35 @@
 <!DOCTYPE html>
 <html lang="sv">
+
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <link href="style.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/1a8500da42.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/styles/header.css">
+    <link rel="stylesheet" href="src/styles/blog.css">
+    <link rel="stylesheet" href="src/styles/main.css">
     <title><?php echo TITLE; ?></title>
 </head>
+
 <body>
-<div class="main_container">
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="signup.php">Sign up</a></li>
-            <li><a href="login.php">Login</a></li>
-        </ul>
-    </nav>
-    
+    <div class="main_container">
+        <header id="main-header">
+            <div class="logo">
+                <h1 class="logo-title">LogoTitle</h1>
+            </div>
+            <nav>
+                <ul class="navigation">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="signup.php">Sign up</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="#">Logged in user</a>
+                        <ul>
+                            <li><a href="#">Dashboard</a></li>
+                            <li><a href="#" class="text-danger">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </header>
