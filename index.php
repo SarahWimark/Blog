b73497cab3685896bb263d34a8e276c484fa4091
sarahwimark <sarahwimark@gmail.com>
2022-03-25 10:45:28 +0100

    Adding a carousel for newest posts

--- index.php
+++ index.php
@@ -2,7 +2,20 @@
 <?php include_once('./src/includes/header.php'); ?>
 
 <main>
-    <div>
+    <div class="carousel-container">
+
+        <div class="new-posts-carousel"></div>
+        <h1 class="carousel-title">Newest posts</h1>
+        <i class="fa fa-chevron-left prev"></i>
+        <i class="fa fa-chevron-right next"></i>
+        <div class="new-posts-container">
+            <div class="new-post">1</div>
+            <div class="new-post">2</div>
+            <div class="new-post">3</div>
+            <div class="new-post">4</div>
+            <div class="new-post">5</div>
+        </div>
+
     </div>
 </main>
 <?php include_once('./src/includes/footer.php'); ?>
---
+++ src/js/carousel.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".new-posts-container").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+  });
+});
