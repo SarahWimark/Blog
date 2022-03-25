20dd68541c45d667a8d19f4de460d26174acbf3a
sarahwimark <sarahwimark@gmail.com>
2022-03-25 17:09:04 +0100

    Styling the sidebar

--- src/blog/main.php
+++ src/blog/main.php
@@ -12,8 +12,84 @@
                     aspernatur laudantium
                     inventore voluptas eius accusantium, ea cum! Odio.</p>
                 <i class="far fa-user"> Bloggers name</i>
+                <div>
+                    <a href="#" class="btn btn-outline-secondary blog-card-btn" role="button" aria-pressed="true">Go to
+                        blog</a>
+                </div>
+            </div>
+        </div>
+        <div class="blog-card">
+            <img src="src/images/blogs/blog_header.png"
+                alt="A blog header image that says Explore the world and with a camera" class="blog-card-img">
+            <div class="blog-card-content">
+                <h2 class="blog-card-title"><a href="#"></a>Lorem ipsum dolor sit amet</h2>
+                <p class="blog-card-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem delectus
+                    nisi
+                    aspernatur laudantium
+                    inventore voluptas eius accusantium, ea cum! Odio.</p>
+                <i class="far fa-user"> Bloggers name</i>
+                <div>
+                    <a href="#" class="btn btn-outline-secondary blog-card-btn" role="button" aria-pressed="true">Go to
+                        blog</a>
+                </div>
+            </div>
+        </div>
+        <div class="blog-card">
+            <img src="src/images/blogs/blog_header.png"
+                alt="A blog header image that says Explore the world and with a camera" class="blog-card-img">
+            <div class="blog-card-content">
+                <h2 class="blog-card-title"><a href="#"></a>Lorem ipsum dolor sit amet</h2>
+                <p class="blog-card-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem delectus
+                    nisi
+                    aspernatur laudantium
+                    inventore voluptas eius accusantium, ea cum! Odio.</p>
+                <i class="far fa-user"> Bloggers name</i>
+                <div>
+                    <a href="#" class="btn btn-outline-secondary blog-card-btn" role="button" aria-pressed="true">Go to
+                        blog</a>
+                </div>
+            </div>
+        </div>
+        <div class="blog-card">
+            <img src="src/images/blogs/blog_header.png"
+                alt="A blog header image that says Explore the world and with a camera" class="blog-card-img">
+            <div class="blog-card-content">
+                <h2 class="blog-card-title"><a href="#"></a>Lorem ipsum dolor sit amet</h2>
+                <p class="blog-card-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem delectus
+                    nisi
+                    aspernatur laudantium
+                    inventore voluptas eius accusantium, ea cum! Odio.</p>
+                <i class="far fa-user"> Bloggers name</i>
+                <div>
+                    <a href="#" class="btn btn-outline-secondary blog-card-btn" role="button" aria-pressed="true">Go to
+                        blog</a>
+                </div>
             </div>
         </div>
     </div>
-    <div class="sidebar"></div>
+    <aside class="sidebar">
+        <section class="search">
+            <h2 class="search-title">Search</h2>
+            <form action="">
+                <input type="text" name="searchterm" class="search-input" placeholder="Search...">
+            </form>
+        </section>
+
+        <section class="categories">
+            <h2 class="categories-title">Categories</h2>
+            <ul>
+                <li><a href="">Category 1</a></li>
+                <li><a href="">Category 2</a></li>
+                <li><a href="">Category 3</a></li>
+                <li><a href="">Category 4</a></li>
+                <li><a href="">Category 5</a></li>
+                <li><a href="">Category 6</a></li>
+                <li><a href="">Category 7</a></li>
+                <li><a href="">Category 8</a></li>
+                <li><a href="">Category 9</a></li>
+                <li><a href="">Category 10</a></li>
+            </ul>
+        </section>
+
+    </aside>
 </div>
