e600357010f7545eabc5b3b6fca08990b6e2d0b5
sarahwimark <sarahwimark@gmail.com>
2022-03-25 11:47:55 +0100

    Styling new posts carousel

--- index.php
+++ index.php
@@ -6,14 +6,64 @@
 
         <div class="new-posts-carousel"></div>
         <h1 class="carousel-title">Newest posts</h1>
-        <i class="fa fa-chevron-left prev"></i>
-        <i class="fa fa-chevron-right next"></i>
+        <i class="fas fa-chevron-left prev"></i>
+        <i class="fas fa-chevron-right next"></i>
         <div class="new-posts-container">
-            <div class="new-post">1</div>
-            <div class="new-post">2</div>
-            <div class="new-post">3</div>
-            <div class="new-post">4</div>
-            <div class="new-post">5</div>
+            <div class="new-post">
+                <img src="src/images/unsplash.jpg" alt="An image of rocky mountains in a desert plain"
+                    class="carousel-img">
+                <div class="post-info">
+                    <h4><a href="#">Blog post title</a></h4>
+                    <i class="far fa-user"> Blog post author</i> &nbsp;
+                    <i class="far fa-calendar"> Mar 25, 2022</i>
+                </div>
+            </div>
+            <div class="new-post">
+                <img src="src/images/unsplash.jpg" alt="An image of rocky mountains in a desert plain"
+                    class="carousel-img">
+                <div class="post-info">
+                    <h4><a href="#">Blog post title</a></h4>
+                    <i class="far fa-user"> Blog post author</i> &nbsp;
+                    <i class="far fa-calendar"> Mar 25, 2022</i>
+                </div>
+            </div>
+            <div class="new-post">
+                <img src="src/images/unsplash.jpg" alt="An image of rocky mountains in a desert plain"
+                    class="carousel-img">
+                <div class="post-info">
+                    <h4><a href="#">Blog post title</a></h4>
+                    <i class="far fa-user"> Blog post author</i> &nbsp;
+                    <i class="far fa-calendar"> Mar 25, 2022</i>
+                </div>
+            </div>
+            <div class="new-post">
+                <img src="src/images/unsplash.jpg" alt="An image of rocky mountains in a desert plain"
+                    class="carousel-img">
+                <div class="post-info">
+                    <h4><a href="#">Blog post title</a></h4>
+                    <i class="far fa-user"> Blog post author</i> &nbsp;
+                    <i class="far fa-calendar"> Mar 25, 2022</i>
+                </div>
+            </div>
+            <div class="new-post">
+                <img src="src/images/unsplash.jpg" alt="An image of rocky mountains in a desert plain"
+                    class="carousel-img">
+                <div class="post-info">
+                    <h4><a href="#">Blog post title</a></h4>
+                    <i class="far fa-user"> Blog post author</i> &nbsp;
+                    <i class="far fa-calendar"> Mar 25, 2022</i>
+                </div>
+            </div>
+            <div class="new-post">
+                <img src="src/images/unsplash.jpg" alt="An image of rocky mountains in a desert plain"
+                    class="carousel-img">
+                <div class="post-info">
+                    <h4><a href="#">Blog post title</a></h4>
+                    <i class="far fa-user"> Blog post author</i> &nbsp;
+                    <i class="far fa-calendar"> Mar 25, 2022</i>
+                </div>
+            </div>
+
         </div>
 
     </div>
--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,8 +1,38 @@
 $(document).ready(function () {
   $(".new-posts-container").slick({
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 10000,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
   });
+
+  /*  $(".new-posts-container").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  }); */
 });
