09d7653966c65684344f4ebde94d3bc89175e608
sarahwimark <sarahwimark@gmail.com>
2022-03-26 22:39:21 +0100

    Working on blog post

--- src/blog/includes/content.php
+++ src/blog/includes/content.php
@@ -2,26 +2,59 @@
 <?php include_once('../../includes/header.php'); ?>
 
 <main>
-    <div class="post-content">
-        <h2 class="post-title">This is the title of the post</h2>
-    </div>
+    <div class="content post-content clearfix">
+        <div class="blog-post-section">
+            <h2 class="post-title">This is the title of the post</h2>
+            <div class="post-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ad ea officia cumque sequi modi
+                    debitis eaque nihil. Quisquam consequatur ex soluta quis unde eius at quibusdam laborum et optio.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus deleniti mollitia reprehenderit
+                    placeat culpa, id non, recusandae porro odio eveniet, ipsa laboriosam hic! Illum veritatis aperiam
+                    dolorem nobis inventore ullam, quia quaerat provident, vitae repellendus magni ab, quo adipisci at
+                    mollitia. Ullam iste pariatur repellat est veritatis repudiandae corrupti consequatur?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ullam quam libero doloremque
+                    eligendi fugiat ipsum culpa laborum distinctio vero non tempore. Nemo in architecto unde quis ipsam
+                    maiores repellat, autem nobis dolore ipsa corporis consequatur rem nam. Explicabo tempore enim
+                    numquam, totam sapiente quia incidunt adipisci accusamus quis doloribus, animi ullam blanditiis
+                    minus. Illo ducimus laboriosam vel nulla obcaecati iusto facilis accusantium velit dignissimos rerum
+                    reiciendis nesciunt, minima optio vero aspernatur! Non dolores placeat, voluptates at mollitia rerum
+                    accusamus laboriosam iste officiis obcaecati perspiciatis. Sint doloremque facere consequatur
+                    corrupti omnis, a voluptates suscipit nemo alias molestias voluptas dignissimos illum!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est nostrum cum libero harum, illum in
+                    obcaecati asperiores esse consequuntur quae quo! Non perferendis, dolor dolore explicabo eaque
+                    deserunt dignissimos sapiente cumque doloribus harum modi ipsam dolorum illum quis ea, ad delectus,
+                    odit porro. Adipisci modi non id assumenda iure sequi?</p>
+                <img src="src/images/uploads/unsplash.jpg"
+                    alt="A blog header image that says Explore the world and with a camera" class="post-image">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ad ea officia cumque sequi modi
+                    debitis eaque nihil. Quisquam consequatur ex soluta quis unde eius at quibusdam laborum et optio.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus deleniti mollitia reprehenderit
+                    placeat culpa, id non, recusandae porro odio eveniet, ipsa laboriosam hic! Illum veritatis aperiam
+                    dolorem nobis inventore ullam, quia quaerat provident, vitae repellendus magni ab, quo adipisci at
+                    mollitia. Ullam iste pariatur repellat est veritatis repudiandae corrupti consequatur?</p>
 
-    <aside class="sidebar right">
-        <section class="categories">
-            <h2 class="categories-title">Categories</h2>
-            <ul>
-                <li><a href="">Category 1</a></li>
-                <li><a href="">Category 2</a></li>
-                <li><a href="">Category 3</a></li>
-                <li><a href="">Category 4</a></li>
-                <li><a href="">Category 5</a></li>
-                <li><a href="">Category 6</a></li>
-                <li><a href="">Category 7</a></li>
-                <li><a href="">Category 8</a></li>
-                <li><a href="">Category 9</a></li>
-                <li><a href="">Category 10</a></li>
-            </ul>
-        </section>
-    </aside>
+            </div>
+        </div>
+
+        <aside class="sidebar right post-sidebar">
+            <section class="categories">
+                <h2 class="categories-title">Categories</h2>
+                <ul>
+                    <li><a href="">Category 1</a></li>
+                    <li><a href="">Category 2</a></li>
+                    <li><a href="">Category 3</a></li>
+                    <li><a href="">Category 4</a></li>
+                    <li><a href="">Category 5</a></li>
+                    <li><a href="">Category 6</a></li>
+                    <li><a href="">Category 7</a></li>
+                    <li><a href="">Category 8</a></li>
+                    <li><a href="">Category 9</a></li>
+                    <li><a href="">Category 10</a></li>
+                </ul>
+            </section>
+        </aside>
+    </div>
 </main>
 <?php include_once('../../includes/footer.php'); ?>
