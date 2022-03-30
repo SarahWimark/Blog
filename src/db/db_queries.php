cc3a5e57c81cfa1654e38dc0988bcf07dc9e718e
sarahwimark <sarahwimark@gmail.com>
2022-03-30 20:55:03 +0200

    Adding query to get a user by id

--- src/db/db_queries.php
+++ src/db/db_queries.php
@@ -11,8 +11,19 @@ function getAllUsers($table) {
     return $result;
 }
 
-function printQueryResult($result) {
-    echo "<pre>",print_r($result),"</pre>";
+function getUserById($table, $id) {
+    global $conn;
+    $sql = "SELECT * FROM $table WHERE id = $id";
+    $statement = $conn->prepare($sql);
+    $statement->execute();
+    $result = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
+    return $result;
 }
 
-printQueryResult(getAllUSers('users'));
+function printQueryResult($result) {
+    echo "<pre>",print_r($result, true),"</pre>";
+    die();
+}
+
+//printQueryResult(getAllUSers('users'));
+printQueryResult(getUserById('users', 1));
