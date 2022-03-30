52c54b6d1833318d78471cce106ace03eec1a5f4
sarahwimark <sarahwimark@gmail.com>
2022-03-30 21:17:42 +0200

    .

--- src/db/db_queries.php
+++ src/db/db_queries.php
@@ -16,15 +16,18 @@ function getUserById($table, $id) {
     $sql = "SELECT * FROM $table WHERE id = $id";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
-    $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    $result = $stmt->get_result()->fetch_assoc();
     return $result;
 }
-   
 
+function executeQuery($sql, $data) {
+    
+}
+   
 function printQueryResult($result) {
     echo "<pre>",print_r($result, true),"</pre>";
     die();
 }
 
-//printQueryResult(getAllUSers('users'));
+// printQueryResult(getAllUSers('users'));
 printQueryResult(getUserById('users', 1));
