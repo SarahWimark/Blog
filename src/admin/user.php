2f61ed23cd396e5d7b666f8df9d9822ad4f4e34d
sarahwimark <sarahwimark@gmail.com>
2022-03-31 21:02:08 +0200

    Working on admin pages

--- src/admin/user.php
+++ src/admin/user.php
@@ -3,11 +3,18 @@
 
 <main class="admin-content">
     <div class="admin-sidebar">
-
+        <ul>
+            <li><a href="#">Manage posts</a></li>
+            <li><a href="#">Manage categories</a></li>
+            <li><a href="#">Manage users</a></li>
+        </ul>
     </div>
 
     <div class="admin-dashboard">
-
+        <div class="manage-buttons">
+            <a href=""></a>
+            <a href=""></a>
+        </div>
     </div>
 </main>
 <?php include_once('../includes/footer-small.php'); ?>
