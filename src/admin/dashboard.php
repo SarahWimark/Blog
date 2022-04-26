81a282c39d8523f5914291e8f1f489bd1d3d7a55
sarahwimark <sarahwimark@gmail.com>
2022-04-26 20:54:36 +0200

    Fixing admin links

--- src/admin/dashboard.php
+++ src/admin/dashboard.php
@@ -1,9 +1,9 @@
 <main class="admin-content">
     <div class="admin-sidebar">
         <ul>
-            <li><a href="post.php">Manage posts</a></li>
-            <li><a href="categories.php">Manage categories</a></li>
-            <li><a href="user.php">Manage users</a></li>
+            <li><a href="src/admin/post.php">Manage posts</a></li>
+            <li><a href="src/admin/categorie.php">Manage categories</a></li>
+            <li><a href="src/admin/user.php">Manage users</a></li>
         </ul>
     </div>
 
